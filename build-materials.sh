f0bc05ab4e397efae1dd5e7bcf14047a0e22c634
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2020-09-10 09:54:16 -0400

    Added comment for matc in build-materials.sh

--- build-materials.sh
+++ build-materials.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+## matc should either built from source or downloaded for you platform from here:
+## https://github.com/google/filament
 for path in ./app/src/main/materials/* ; do
   fullname=$(basename "$path")
   filename="${fullname%.*}"
